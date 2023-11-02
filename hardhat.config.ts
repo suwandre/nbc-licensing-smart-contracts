14ddd6b48200f3f4bbbf2e00bdf6899b219bd871
suwandre <suwandresukijat@gmail.com>
2023-11-02 23:21:59 +0100

    feat: implemented MultiOwnable, a modified version of Ownable

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,15 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.22",
+  solidity: {
+    version: "0.8.22",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    }
+  }
 };
 
 export default config;
